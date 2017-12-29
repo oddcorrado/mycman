7f50a35a95a28e40c23b46a27bf4551ce6922b6b
corrado <corradolongoni@yahoo.com>
2017-12-29 18:26:14 +0100

    powerups ui improvements

--- public/powerup.js
+++ public/powerup.js
@@ -6,7 +6,7 @@ let socket = null
 
 const setSocket = (socketIn) => {
   socket = socketIn
-  socket.on('powerup-update', () => updatePowerups()) // TODO
+  socket.on('powerup-update', (powerups) => updatePowerups(powerups)) // TODO
 }
 
 $('#nav-powerup').on('click', function (e) {
@@ -14,37 +14,50 @@ $('#nav-powerup').on('click', function (e) {
   $('#powerup').html('<h3>POWERUPS</h3>')
   if(!menu.isLeftMenuActive()) { return }
   menu.hideAll()
-  updatePowerups()
+  socket.emit('powerup-get-all', (powerups) => updatePowerups(powerups))
   $('#powerup').show()
 })
 
-const updatePowerups = () => {
-  socket.emit('powerup-get-all', (powerups) => {
-    console.log(powerups)
-    $('#powerup').html('<h3>POWERUPS</h3>')
-    powerups.forEach(powerup => addPowerup(powerup))
-  })
+const updatePowerups = (powerups) => {
+  console.log(powerups)
+  $('#powerup').html('<h3>POWERUPS</h3>')
+  powerups.forEach(powerup => addPowerup(powerup))
 }
 
 const addPowerup = (powerup) => {
   let html = '<div class="powerup-card">'
   html += '<div class="clearfix">'
   html += '<div class="powerup-card-value">' + powerup.name + '</div>'
-  html += '<div class="powerup-card-value">TOUS LES ' + powerup.cooldown + ' TOURS</div>'
-  html += '<div class="powerup-card-value">READY:' + powerup.available + '</div>'
-  html += '<div id="powerup-' + powerup.name + '"class="powerup-card-use">UTILISER</div>'
+
+  html += '<div class="powerup-card-value">TOUS LES ' + powerup.cooldownPeriod + ' TOURS</div>'
+  if( powerup.available) {
+    html += '<div class="powerup-card-value">UTILISABLE</div>'
+  } else {
+    html += '<div class="powerup-card-value">NON REVELE</div>'
+  }
+
+  if(powerup.available) {
+    if(powerup.cooldown <= 0) {
+      html += '<div id="powerup-' + powerup.name + '"class="powerup-card-use">UTILISER</div>'
+    } else {
+      html += '<div class="powerup-card-use">ACTIVABLE DANS ' + powerup.cooldown + ' TOURS</div>'
+    }
+  }
+
   html += '</div>'
   html += '<div class="powerup-card-help">' + powerup.help + '</div>'
   html += '</div>'
   $('#powerup').append(html)
 
-  $('#powerup-' + powerup.name).on('click', () => {
-    console.log('use powerup ' + powerup.name)
-    socket.emit('powerup-use', powerup.name, (result) => {
-      console.log(result)
-      updatePowerups()
+  if(powerup.available && powerup.cooldown <= 0) {
+    $('#powerup-' + powerup.name).on('click', () => {
+      socket.emit('powerup-use', powerup.name, (result) => {
+        console.log(result)
+        //updatePowerups()
+      })
     })
-  })
+  }
+
 }
 
 module.exports = {
